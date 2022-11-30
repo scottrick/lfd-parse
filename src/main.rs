cc925353069fc684d6d867cc48b34909d4e86273
Scott Atkins <scottrick49@gmail.com>
2022-11-29 20:32:26 -0600

    Start of parsing!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
+use std::fs::File;
+use std::io;
+use std::io::Read;
+
+fn main() -> io::Result<()> {
     println!("Hello, world!");
+
+    let mut file = File::open("data/BATTLE1.LFD").expect("should open");
+
+    let mut buffer = [0; 10];
+
+    let read_amount = file.read(&mut buffer[..])?;
+
+    println!("read_amount {read_amount}");
+    println!("read bytes: {:x?}", &buffer[..read_amount]);
+
+    Ok(())
 }
