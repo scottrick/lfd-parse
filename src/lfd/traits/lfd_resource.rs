70a3537abf74ecfc6dc63152824cf6c850a8db3c
Scott Atkins <scottrick49@gmail.com>
2022-12-07 20:24:13 -0600

    Defined to_writer interface.

--- src/lfd/traits/lfd_resource.rs
+++ src/lfd/traits/lfd_resource.rs
@@ -2,6 +2,7 @@ use crate::lfd::lfd_header::LfdHeader;
 
 use std::io::Read;
 use std::io::Seek;
+use std::io::Write;
 
 pub trait LfdResource {
     fn get_lfd_header(&self) -> &LfdHeader;
@@ -9,6 +10,13 @@ pub trait LfdResource {
     fn from_reader(reader: &mut (impl Read + Seek), header: LfdHeader) -> Result<Self, String>
     where
         Self: Sized;
+
+    fn to_writer(&self, _writer: &mut impl Write) -> Result<(), String>
+    where
+        Self: Sized,
+    {
+        Ok(())
+    }
 
     fn lfd_get_print_str(&self) -> String;
 
